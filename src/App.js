515a1e8d48f9a079cc17d5078caf912ac5203b31
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-30 10:26:17 -0600

    Added about component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from './components/About';
 
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
      <h1>Git Branching/Merger Practice</h1>
      <p>Let's Practice Git Branching/Merging</p>
+     <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class About extends React.Component
+{
+    render ()
+    {
+        return(
+            <p>
+                Branches help us work with teams;              
+                maintaining the integrity of our                
+                commit history!
+            </p>
+        );
+    }
+}
+
+export default About;
