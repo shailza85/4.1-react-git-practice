7f465f2b819240c64ceddceaad7fdb0886bbb920
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-30 11:20:28 -0600

    Added text in app.js.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './components/About';
+import Services from './components/Services';
 
 import './App.css';
 
@@ -7,8 +8,9 @@ function App() {
   return (
     <div className="App">
      <h1>Git Branching/Merger Practice</h1>
-     <p>Let's Practice Git Branching/Merging</p>
+     <p>Let's Practice some Git Branching/Merging</p>
      <About />
+     <Services />
     </div>
   );
 }
---
+++ src/components/Services.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Services extends React.Component
+{
+    render ()
+    {
+        return(
+            <>
+            <p>
+                We offer many great services like.....
+                            </p>
+                            <ul>
+                                <li>Web Design</li>
+                                <li>Web Development</li>
+                                <li>Project Management</li>
+                            </ul>
+                            </>
+        );
+    }
+}
+
+export default Services;
